e6b7170f27c186b9fbc243665a8b3318081be304
Josh Krall <krall@JoshMacPro.local>
2010-01-26 20:43:38 -0600

    Named scope of_xxxx should be create regardless of whether the named_scopes option==false

--- lib/magic_enum.rb
+++ lib/magic_enum.rb
@@ -143,11 +143,11 @@ module MagicEnum
             end if opts[:scope_extensions] and opts[:scope_extensions].is_a?(Hash)
           end
         end
-        named_scope "of_#{name}".to_sym, lambda { |t| { :conditions => ["#{name} = ?", const_get(enum)[t]] } } do
-          opts[:scope_extensions].each do |ext_name, ext_block|
-            define_method ext_name, ext_block
-          end if opts[:scope_extensions] and opts[:scope_extensions].is_a?(Hash)
-        end
+      end
+      named_scope "of_#{name}".to_sym, lambda { |t| { :conditions => ["#{name} = ?", const_get(enum)[t]] } } do
+        opts[:scope_extensions].each do |ext_name, ext_block|
+          define_method ext_name, ext_block
+        end if opts[:scope_extensions] and opts[:scope_extensions].is_a?(Hash)
       end
 
     end
